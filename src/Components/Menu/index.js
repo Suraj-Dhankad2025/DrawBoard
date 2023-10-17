79d4c1d68f32bb1e9f43a4e3ad6ee8e97567a05c
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2023-10-17 16:10:56 +0530

    added download feature

--- src/Components/Menu/index.js
+++ src/Components/Menu/index.js
@@ -14,6 +14,9 @@ const Menu = () => {
     const handleMenuClick = (itemName) => {
         dispatch(menuItemClick(itemName))
     }
+    const handleActionClick = (actionName) => {
+        dispatch(actionItemClick(actionName))
+    }
     return (
         <div className={styles.menuContainer}>
             <div className={cx(styles.iconWrapper, {[styles.active]: activeMenuItem === MENU_ITEMS.PENCIL})} onClick={() => handleMenuClick(MENU_ITEMS.PENCIL)}>
@@ -22,16 +25,13 @@ const Menu = () => {
             <div className={cx(styles.iconWrapper, {[styles.active]: activeMenuItem === MENU_ITEMS.ERASER})} onClick={() => handleMenuClick(MENU_ITEMS.ERASER)}>
                 <FontAwesomeIcon icon={faEraser} className={styles.icon} />
             </div>
-            <div className={styles.iconWrapper}>
+            <div className={styles.iconWrapper} onClick={() => handleActionClick(MENU_ITEMS.UNDO)}>
                 <FontAwesomeIcon icon={faRotateLeft} className={styles.icon}/>
             </div>
-            <div className={styles.iconWrapper}>
+            <div className={styles.iconWrapper} onClick={() => handleActionClick(MENU_ITEMS.REDO)}>
                 <FontAwesomeIcon icon={faRotateRight} className={styles.icon}/>
             </div>
-            <div className={styles.iconWrapper}>
-                <FontAwesomeIcon icon={faPencil} className={styles.icon}/>
-            </div>
-            <div className={styles.iconWrapper}>
+            <div className={styles.iconWrapper} onClick={() => handleActionClick(MENU_ITEMS.DOWNLOAD)}>
                 <FontAwesomeIcon icon={faFileArrowDown} className={styles.icon}/>
             </div>
         </div>

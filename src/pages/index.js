069dc67ce206cf4f5c34713dbde7cec5bc4ef7e7
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2023-10-17 13:52:57 +0530

    added complete UI

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Menu from "@/Components/Menu"
+import Toolbox from "@/Components/Toolbox"
+export default function Home() {
+  return (
+    <>
+     <Menu/>
+     <Toolbox/>
+    </>
+  )
+}

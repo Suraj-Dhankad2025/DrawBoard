7db156684bdb46ac792bbafc80694b98e2d11f69
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2023-10-17 17:33:49 +0530

    some changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-import Menu from "@/components/Menu"
-import Toolbox from "@/components/Toolbox"
-import Board from "@/components/Board"
+import Menu from "../components/Menu"
+import Toolbox from "../components/Toolbox"
+import Board from "../components/Board"
 export default function Home() {
   return (
     <>
